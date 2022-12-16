150a863ae134628df4bd4040cbf9223ad25c819e
rishad-e <rishadedassery987@gmail.com>
2022-12-16 18:41:01 +0530

    Drawer and version completed

--- lib/screens/home_screens/Drawer/drawer_home.dart
+++ lib/screens/home_screens/Drawer/drawer_home.dart
@@ -14,81 +14,90 @@ class HomeDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      // backgroundColor: Colors.redAccent,
-      child: ListView(
+      backgroundColor: const Color.fromARGB(255, 42, 5, 90),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          DrawerHeader(
-            child: Image.asset('assets/images/image.png'),
+          SizedBox(
+            child: Column(
+              children: [
+                DrawerHeader(
+                  child: Image.asset('assets/images/image.png'),
+                ),
+                DrawerTile(
+                  ontap1: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const AboutUsDrawer()));
+                  },
+                  icon1: Icons.report_outlined,
+                  text1: "About Us",
+                ),
+                DrawerTile(
+                  ontap1: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const PrivacyDrawer()));
+                  },
+                  icon1: Icons.lock_person_rounded,
+                  text1: "Privacy & Policy",
+                ),
+                DrawerTile(
+                  ontap1: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            backgroundColor:
+                                const Color.fromARGB(223, 43, 9, 53),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                            title: Text('Reset App',
+                                style: GoogleFonts.ubuntuCondensed(
+                                    textStyle: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20))),
+                            content: Text('Are You Sure Want To Reset The App',
+                                style: GoogleFonts.ubuntuCondensed(
+                                    textStyle: const TextStyle(fontSize: 18))),
+                            actions: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  TextButton(
+                                      onPressed: () {
+                                        PlayListDB.resetAPP(context);
+                                        GetSongs.player.stop();
+                                      },
+                                      child: const Text('Reset')),
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text('Cancel'))
+                                ],
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  icon1: Icons.refresh_outlined,
+                  text1: "Reset App",
+                ),
+                DrawerTile(
+                  ontap1: () {
+                    ShareAppFile(context);
+                  },
+                  icon1: Icons.share,
+                  text1: "Share This App",
+                ),
+              ],
+            ),
           ),
-          DrawerTile(
-            ontap1: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const AboutUsDrawer()));
-            },
-            icon1: Icons.report_outlined,
-            text1: "About Us",
-          ),
-          DrawerTile(
-            ontap1: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const PrivacyDrawer()));
-            },
-            icon1: Icons.lock_person_rounded,
-            text1: "Privacy & Policy",
-          ),
-          DrawerTile(
-            ontap1: () {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      backgroundColor: const Color.fromARGB(223, 43, 9, 53),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
-                      title: Text('Reset App',
-                          style: GoogleFonts.ubuntuCondensed(
-                              textStyle: const TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20))),
-                      content: Text('Are You Sure Want To Reset The App',
-                          style: GoogleFonts.ubuntuCondensed(
-                              textStyle: const TextStyle(fontSize: 18))),
-                      actions: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            TextButton(
-                                onPressed: () {
-                                  PlayListDB.resetAPP(context);
-                                  GetSongs.player.stop();
-                                },
-                                child: const Text('Reset')),
-                            TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text('Cancel'))
-                          ],
-                        )
-                      ],
-                    );
-                  });
-            },
-            icon1: Icons.refresh_outlined,
-            text1: "Reset App",
-          ),
-          DrawerTile(
-            ontap1: () {
-              ShareAppFile(context);
-            },
-            icon1: Icons.share,
-            text1: "Share This App",
-          ),
-          // SizedBox(height:MediaQuery.of(context).size.height*0.32),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.32),
           const Text(
-            "version v.1.0.0",
+            "V.1.0.0",
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.grey),
-          )
+          ),
         ],
       ),
     );
